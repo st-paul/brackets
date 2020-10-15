5d5a15b845555033746c54cb117b05d11549e17a
st-paul <statkevich.pa@gmail.com>
2020-10-15 14:13:06 +0300

    feat: implement task bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0 ) return false;
+    let arr = str.split('');
+    let resultArr = [];
+    arr.forEach(element => {
+        if (element !== resultArr[resultArr.length - 1]) {
+            let brackConf = bracketsConfig.find(el => el[0] == element);
+            if (brackConf == undefined) return false;
+            resultArr.push(brackConf[1]);
+        } else {
+            resultArr.pop();
+        }
+    });
+    return resultArr.length == 0 ? true : false;
 }
